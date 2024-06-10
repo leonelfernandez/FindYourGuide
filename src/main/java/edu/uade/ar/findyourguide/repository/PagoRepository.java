54e78895f8af0e0c16199a978d9a823b090110ca
Leonel Fernandez <leofranferros@gmail.com>
2024-06-10 18:12:35 -0300

    add: Tags faltantes para iny dep

--- src/main/java/edu/uade/ar/findyourguide/repository/PagoRepository.java
+++ src/main/java/edu/uade/ar/findyourguide/repository/PagoRepository.java
@@ -2,6 +2,8 @@ package edu.uade.ar.findyourguide.repository;
 
 import edu.uade.ar.findyourguide.model.entity.PagoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PagoRepository extends JpaRepository<PagoEntity, Long> {
 }
--- src/main/java/edu/uade/ar/findyourguide/repository/ReservaRepository.java
+++ src/main/java/edu/uade/ar/findyourguide/repository/ReservaRepository.java
@@ -2,6 +2,8 @@ package edu.uade.ar.findyourguide.repository;
 
 import edu.uade.ar.findyourguide.model.entity.ReservaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservaRepository extends JpaRepository<ReservaEntity, Long> {
 }
